4c0b4603d8952650a37d962942627d4f62f7ebca
Samuel Jack Hill <whatinthesamhill@protonmail.com>
2024-06-23 17:08:26 -0500

    Update Benchmark.cs
    people list in people class

--- Assets/Scripts/Simulator/Benchmark.cs
+++ Assets/Scripts/Simulator/Benchmark.cs
@@ -45,9 +45,6 @@ namespace Scripts.Simulator {
             WriteSaveData = writer => writer.Write(ClockTick.ToString());
         }
 
-        private static readonly List<Person> People = new();
-        private static readonly List<Location> Places = new();
-
         // ReSharper disable InconsistentNaming
         // Tables, despite being local or private variables, will be capitalized for style/identification purposes.
 
