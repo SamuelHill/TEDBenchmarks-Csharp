7c91a673b481f9378033329716c50954410cc4a9
Ian Horswill <ian@northwestern.edu>
2024-06-23 17:09:09 -0500

    Began C#-specific code for Person, Location

---
+++ Assets/Scripts/Simulator/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Scripts.ValueTypes
+{
+    /// <summary>
+    /// Members of Person that are specific to the C# implementation of the benchmark
+    /// </summary>
+    public partial class Person : IComparable<Person>, IEquatable<Person>
+    {
+        public static readonly List<Person> Everyone = new List<Person>();
+
+        public static void CreateAll(int count)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                Everyone.Add(new Person("Bob", $"Mc{i}", TED.Utilities.Random.MakeRng()));
+            }
+        }
+
+        private Location _location;
+
+        public Location Location
+        {
+            get => _location;
+            set
+            {
+                if (_location == value)
+                    return;
+                if (!ReferenceEquals(_location, null))
+                    _location.Occupants.Remove(this);
+                _location = value;
+                _location.Occupants.Add(this);
+            }
+        }
+    }
+}
